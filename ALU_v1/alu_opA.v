1a5ea6ef1ad53a43ea36cc8302d3bb4ef57a83ee
Varun Ganeshan <94780009+VarunGaneshan@users.noreply.github.com>
2025-06-03 10:02:10 +0530

    Add files via upload

---
+++ ALU_v1/alu_opA.v
@@ -0,0 +1,47 @@
+`include "defines.v"
+module alu_opA (
+    	enable, mode, cmd, opa, res, cout, oflow
+);
+    	input  wire                  enable;
+    	input  wire                  mode;
+    	input  wire [`CMD_WIDTH-1:0] cmd;
+    	input  wire [`OP_WIDTH-1:0]  opa;
+
+        output reg [`OP_WIDTH:0]     res;
+    	output reg                   cout;
+    	output reg                   oflow;
+
+always @ (*) begin //{
+	res = 0;
+	cout = 0;
+	oflow = 0;
+	if (enable) begin //{	
+		if (mode) begin //{
+			case (cmd)
+				`INC_A : begin //{
+						res[`OP_WIDTH:0] = opa + 1;
+						cout = res[`OP_WIDTH];
+					 end //}
+
+				default : begin //{ 
+				        //`DEC_A
+						res[`OP_WIDTH:0] = opa - 1;
+						oflow = (opa == 0);
+					 end //}
+			endcase  
+		end //}
+		else begin //{
+			case (cmd) 
+				`NOT_A  : res[`OP_WIDTH-1:0] = ~opa; 
+				`SHR1_A : res[`OP_WIDTH-1:0] = opa >> 1;
+		        default : res[`OP_WIDTH-1:0] = opa << 1;	// `SHL1_A
+			endcase 
+		end //}
+	end //}
+	else begin //{
+        	res = 0;
+        	cout = 0;
+        	oflow = 0;
+	end //}
+end //}
+endmodule
---
+++ ALU_v1/alu_opB.v
@@ -0,0 +1,47 @@
+`include "defines.v"
+module alu_opB (
+    	enable, mode, cmd, opb, res, cout, oflow
+);
+    	input  wire                   enable;
+    	input  wire                   mode;
+    	input  wire [`CMD_WIDTH-1:0]  cmd;
+    	input  wire [`OP_WIDTH-1:0]   opb;
+
+    	output reg [`OP_WIDTH:0]      res;
+    	output reg                    cout;
+    	output reg                    oflow;
+
+always @ (*) begin //{
+	res = 0;
+	cout = 0;
+	oflow = 0;
+	if (enable) begin //{	
+		if (mode) begin //{
+			case (cmd)
+				`INC_B : begin //{
+						res[`OP_WIDTH:0] = opb + 1;
+						cout = res[`OP_WIDTH];
+					 end //}
+
+				default : begin //{
+				        //`DEC_B
+						res[`OP_WIDTH:0] = opb - 1;
+						oflow = (opb == 0);
+					 end //}
+			endcase  
+		end //}
+		else begin //{
+			case (cmd) 
+				`NOT_B  : res[`OP_WIDTH-1:0] = ~opb;
+				`SHR1_B : res[`OP_WIDTH-1:0] = opb >> 1;
+		        default : res[`OP_WIDTH-1:0] = opb << 1;	//`SHL1_B
+			endcase 
+		end //}
+	end //}
+	else begin //{
+        	res = 0;
+        	cout = 0;
+        	oflow = 0;
+	end //}
+end //}
+endmodule
